798326c9e05275191dcab0b1c1fd224c98353724
Drick <ampomahderrick9@gmail.com>
2023-09-06 01:23:07 +0000

    Added a new page-- the 'About' Page.

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Abouthead from "../../components/About/About";
+import AboutBody from "../../components/AboutBody/AboutBody";
+import { Nav } from '../../components'
+export default function About () {
+  return (
+
+
+    <div >
+      <div className="container" >
+        <Nav />
+      </div>
+
+      <div>
+        <Abouthead />
+      </div>
+      <AboutBody />
+    </div>
+  )
+}
