b286f311d3976b798ead9c45fa3d8d5cf4ffd70d
Drick <ampomahderrick9@gmail.com>
2023-09-06 01:29:39 +0000

    imported new page and component in various js

--- src/components/index.js
+++ src/components/index.js
@@ -5,14 +5,17 @@ import Services from "./Services/Services";
 import AboutSection from "./AboutSection/AboutSection";
 import Feedback from "./Feedback/Feedback";
 import SubscribeSec from "./SubscribeSec/SubscribeSec";
-
+import About from "./About/About";
+import AboutBody from "./AboutBody/AboutBody";
 
 export {
     Nav,
     Hero,
-    Footer, 
+    Footer,
     Services,
     AboutSection,
     SubscribeSec,
     Feedback,
+    About,
+    AboutBody,
 }
